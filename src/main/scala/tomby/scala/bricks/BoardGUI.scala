4c534e53a1aa20ee9c6cea216764591d721c85bc
Antonio Muñoz <antonio.munoz@rumbo.com>
2018-01-06 19:14:25 +0100

    upgraded to scalafx

--- src/main/scala/tomby/scala/bricks/BoardGUI.scala
+++ src/main/scala/tomby/scala/bricks/BoardGUI.scala
@@ -1,88 +1,58 @@
 package tomby.scala.bricks
 
-import javafx.application.Application
-import javafx.event.ActionEvent
-import javafx.event.EventHandler
-import javafx.geometry.Insets
-import javafx.geometry.Pos
-import javafx.scene.Group
-import javafx.scene.Scene
-import javafx.scene.control.Button
-import javafx.scene.input.MouseEvent
-import javafx.scene.layout.VBoxBuilder
-import javafx.scene.shape.Rectangle
-import javafx.scene.text.Text
-import javafx.stage.Modality
-import javafx.stage.Stage
-import javafx.scene.paint.Color
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
+import scalafx.scene.control.Alert
+import scalafx.scene.control.Alert.AlertType
+import scalafx.scene.layout.Pane
+import scalafx.scene.input.MouseEvent
 
-class BoardGUI extends Application {
+object BoardGUI extends JFXApp {
 
   private val colors = Array("R", "G", "B", "Y")
   private implicit val generator: Position => String = new ColorGenerator(colors).randomColor
   private val board: BoardDSL = new Board(15, 10)
-  private val size = 20
-  private val height = board.height * size;
-  private val width = board.width * size;
-  private val padding = size * 2
-
-  override def start(primaryStage: Stage) = {
-    primaryStage.setTitle("Click'em all!!")
-
-    val root = new Group
-    val scene = new Scene(root, width + padding, height + padding , Color.WHITE)
-
-    root.getChildren.clear()
-    paint().foreach(root.getChildren().add(_))
-
-    scene.setOnMouseClicked(
-      new EventHandler[MouseEvent] {
-        override def handle(event: MouseEvent) {
-          val x = (event.getSceneX() / size).intValue - 1
-          val y = board.height - (event.getSceneY() / size).intValue
-          if (board.click(x, y)) {
-            gameover()
-          }
-          root.getChildren.clear()
-          paint().foreach(root.getChildren().add(_))
-        }
-      })
-
-    primaryStage.setScene(scene)
-    primaryStage.show()
+  private val _size = 20
+  private val _height = board.height * _size;
+  private val _width = board.width * _size;
+  private val _padding = _size * 2
+  
+  private val pane = new Pane {
+    children = paint()
+  }
+  
+  pane.handleEvent(MouseEvent.MouseClicked) {
+    event: MouseEvent => {
+      val _x = (event.getSceneX / _size).toInt - 1
+      val _y = board.height - (event.getSceneY / _size).toInt
+      if (board.click(_x, _y)) {
+        gameover()
+      }
+      pane.children = paint()
+    }
+  }
+  
+  stage = new JFXApp.PrimaryStage {
+    title.value = "Click'em all!!"
+    scene = new Scene(_width + _padding, _height + _padding) {
+      fill = Color.White
+      root = pane
+    }
   }
 
   def gameover() {
-    val dialogStage = new Stage()
-    dialogStage.initModality(Modality.WINDOW_MODAL)
-
-    val builder = VBoxBuilder.create()
-    val yes = new Button("Yeah!")
-    builder.children(new Text("GAME OVER!"), yes)
-    builder.alignment(Pos.CENTER)
-    builder.padding(new Insets(5))
-
-    yes.setOnAction(
-      new EventHandler[ActionEvent] {
-        override def handle(event: ActionEvent) {
-          dialogStage.close()
-        }
-      })
-
-    dialogStage.setScene(new Scene(builder.build()))
-    dialogStage.show();
-  }
-
-  def getColor(position: Position): Color = {
-    board.atPosition(position.x, position.y).fold(Color.WHITE)(toColor)
+    new Alert(AlertType.Information, "GAME OVER!").showAndWait()
   }
   
   def toColor(tile: Tile): Color = {
     tile.color match {
-      case "R" => Color.RED
-      case "G" => Color.GREEN
-      case "B" => Color.BLUE
-      case "Y" => Color.YELLOW
+      case "R" => Color.Red
+      case "G" => Color.Green
+      case "B" => Color.Blue
+      case "Y" => Color.Yellow
     }
   }
 
@@ -91,15 +61,12 @@ class BoardGUI extends Application {
   }
   
   private def toRectangle(tile: Tile): Rectangle = {
-    val rectangle = new Rectangle(size, size, getColor(tile.position))
-    rectangle.setX(size + (tile.position.x * size))
-    rectangle.setY(height - (tile.position.y * size))
-    rectangle
-  }
-}
-
-object BoardGUI {
-  def main(args: Array[String]) {
-    Application.launch(classOf[BoardGUI], args: _*)
+    new Rectangle {
+      x = _size + (tile.position.x * _size)
+      y = _height - (tile.position.y * _size)
+      width = _size
+      height = _size
+      fill = toColor(tile)
+    }
   }
 }
